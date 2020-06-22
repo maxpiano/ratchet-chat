4563d5476a8e245110d82e593c77f5caba965067
Max B <maxbilov@gmail.com>
2020-06-22 16:54:25 +0200

    initial

---
+++ script.js
@@ -0,0 +1,63 @@
+// Socket Server
+var conn = new WebSocket('ws://localhost:8080');
+conn.onopen = function (e) {
+    console.log("Connection established!");
+};
+(function () {
+    var Message;
+    Message = function (arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function (_this) {
+            return function () {
+                var $message;
+                $message = $($('.message_template').clone().html());
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                return setTimeout(function () {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
+    $(function () {
+        var getMessageText, message_side, sendMessage;
+        message_side = 'right';
+        getMessageText = function () {
+            var $message_input;
+            $message_input = $('.message_input');
+            conn.send($message_input.val());
+            return $message_input.val();
+        };
+        sendMessage = function (text, message_side) {
+            var $messages, message;
+            if (text.trim() === '') {
+                return;
+            }
+            $('.message_input').val('');
+            $messages = $('.messages');
+            message_side = message_side || 'left';
+            message = new Message({
+                text: text,
+                message_side: message_side
+            });
+            message.draw();
+            return $messages.animate(
+                {scrollTop: $messages.prop('scrollHeight')},
+                300
+            );
+        };
+        $('.send_message').click(function (e) {
+            return sendMessage(getMessageText());
+        });
+        $('.message_input').keyup(function (e) {
+            if (e.which === 13) {
+                return sendMessage(getMessageText());
+            }
+        });
+        conn.onmessage = function (e) {
+            console.log(e.data);
+            sendMessage(e.data, 'right');
+        };
+    });
+}.call(this));
